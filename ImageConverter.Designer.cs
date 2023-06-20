03eb6119992ad834ba0442b5a15458467876df91
Naem <74654331+NaimShafiq@users.noreply.github.com>
2023-06-20 19:21:22 +0800

    browse file n folder

--- ImageConverter.Designer.cs
+++ ImageConverter.Designer.cs
@@ -35,6 +35,7 @@ namespace ImageFormatConverter
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -87,7 +88,7 @@ namespace ImageFormatConverter
             // 
             this.textBox2.Location = new System.Drawing.Point(44, 31);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(335, 20);
+            this.textBox2.Size = new System.Drawing.Size(269, 20);
             this.textBox2.TabIndex = 5;
             this.textBox2.Text = "Image Path....";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -98,15 +99,26 @@ namespace ImageFormatConverter
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 21);
             this.button2.TabIndex = 6;
-            this.button2.Text = "Browse";
+            this.button2.Text = "Folder";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(319, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 21);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // ImageConverter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 263);
+            this.ClientSize = new System.Drawing.Size(511, 263);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -128,6 +140,7 @@ namespace ImageFormatConverter
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ImageConverter.cs
+++ ImageConverter.cs
@@ -31,11 +31,35 @@ namespace ImageFormatConverter
 
         private void button2_Click(object sender, EventArgs e)
         {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            // Set initial folder
+            folderBrowserDialog.SelectedPath = @"C:\";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Folder path selected by the user
+                string selectedFolderPath = folderBrowserDialog.SelectedPath;
+
+                // Do something with the selected folder path
+                // For example, display it in a text box
+                textBox2.Text = selectedFolderPath;
+            }
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
             OpenFileDialog openFileDialog = new OpenFileDialog();
 
             // Set initial directory and filter for file selection
             openFileDialog.InitialDirectory = @"C:\";
-            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog.Filter = "Text Files (*.png)|*.png|All Files (*.*)|*.*";
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
@@ -46,11 +70,6 @@ namespace ImageFormatConverter
                 // For example, display it in a text box
                 textBox2.Text = selectedFilePath;
             }
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
