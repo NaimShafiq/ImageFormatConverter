fa87d301f0eb24c3210d007b51efff3994f352a1
Naem <74654331+NaimShafiq@users.noreply.github.com>
2023-06-20 19:39:06 +0800

    latest

--- ImageConverter.Designer.cs
+++ ImageConverter.Designer.cs
@@ -29,7 +29,6 @@ namespace ImageFormatConverter
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -38,43 +37,24 @@ namespace ImageFormatConverter
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "JPG",
-            "JPEG",
-            "PNG",
-            "TIF"});
-            this.comboBox1.Location = new System.Drawing.Point(44, 72);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(156, 21);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.TabStop = false;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // comboBox2
             // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "JPG",
-            "JPEG",
-            "PNG",
-            "TIF"});
-            this.comboBox2.Location = new System.Drawing.Point(223, 72);
+            this.comboBox2.Location = new System.Drawing.Point(44, 72);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(156, 21);
+            this.comboBox2.Size = new System.Drawing.Size(269, 21);
             this.comboBox2.TabIndex = 2;
-            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(395, 71);
+            this.button1.Location = new System.Drawing.Point(319, 71);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.Size = new System.Drawing.Size(151, 21);
             this.button1.TabIndex = 3;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -83,6 +63,7 @@ namespace ImageFormatConverter
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(426, 127);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -124,7 +105,6 @@ namespace ImageFormatConverter
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
             this.Name = "ImageConverter";
             this.Text = "Image Format Converter";
             this.ResumeLayout(false);
@@ -133,8 +113,6 @@ namespace ImageFormatConverter
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
--- ImageConverter.cs
+++ ImageConverter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,16 +19,8 @@ namespace ImageFormatConverter
         public ImageConverter()
         {
             InitializeComponent();
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.Items.AddRange(new object[] { "JPG", "JPEG", "PNG", "TIF" });
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -70,6 +64,95 @@ namespace ImageFormatConverter
                 // For example, display it in a text box
                 textBox2.Text = selectedFilePath;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the selected data type from the ComboBox
+            string selectedDataType = comboBox2.SelectedItem.ToString();
+
+            // Validate if a data type is selected
+            if (!string.IsNullOrEmpty(selectedDataType))
+            {
+                // Get the path of the selected image file
+                string selectedImagePath = textBox2.Text;
+
+                // Validate if an image file is selected
+                if (!string.IsNullOrEmpty(selectedImagePath))
+                {
+                    try
+                    {
+                        // Convert the image to the selected data type
+                        string convertedImagePath = ConvertImageToDataType(selectedImagePath, selectedDataType);
+
+                        // Display the converted image path in a text box or perform any other desired action
+                        textBox1.Text = convertedImagePath;
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle any exceptions that occur during the conversion process
+                        MessageBox.Show("Error occurred during image conversion: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select an image file.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a data type.");
+            }
+        }
+
+
+        private string ConvertImageToDataType(string imagePath, string dataType)
+        {
+            // Generate a new file name for the converted image based on the selected data type
+            string convertedImagePath = GetConvertedImagePath(imagePath, dataType);
+
+            // Load the original image
+            using (Image image = Image.FromFile(imagePath))
+            {
+                // Save the image in the desired format
+                switch (dataType)
+                {
+                    case "jpg":
+                        image.Save(convertedImagePath, ImageFormat.Jpeg);
+                        break;
+                    case "png":
+                        image.Save(convertedImagePath, ImageFormat.Png);
+                        break;
+                    case "jpeg":
+                        image.Save(convertedImagePath, ImageFormat.Jpeg);
+                        break;
+                    case "tif":
+                        image.Save(convertedImagePath, ImageFormat.Tiff);
+                        break;
+                    default:
+                        // Unsupported data type
+                        throw new NotSupportedException("Selected data type is not supported.");
+                }
+            }
+
+            // Return the path of the converted image
+            return convertedImagePath;
+        }
+
+        private string GetConvertedImagePath(string originalImagePath, string dataType)
+        {
+            // Generate a new file name for the converted image based on the selected data type
+            string originalFileName = Path.GetFileNameWithoutExtension(originalImagePath);
+            string originalExtension = Path.GetExtension(originalImagePath);
+            string newFileName = originalFileName + "." + dataType;
+            string convertedImagePath = Path.Combine(Path.GetDirectoryName(originalImagePath), newFileName);
+
+            return convertedImagePath;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
